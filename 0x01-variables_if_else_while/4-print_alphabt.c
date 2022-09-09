af6640e78b4164ed86ca04683ae4fd1cdcbdae11
Dking224 <davidshoyemmy2005@gmail.com>
2022-09-09 15:22:16 -0700

    Alpaha

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,15 +8,11 @@
 int main(void)
 {
 	char i;
-	char j;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
+		if (i != 'q' && i != 'e')
 		putchar(i);
-	}
-	for (j = 'A'; j <= 'Z'; j++)
-	{
-		putchar(j);
 	}
 	putchar('\n');
 	return (0);
