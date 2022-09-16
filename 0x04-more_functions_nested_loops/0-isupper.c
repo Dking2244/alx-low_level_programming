d7ae38c011b1aed0edbb03a0f78b1c33ed6cd7d7
Dking2244 <111756648+Dking2244@users.noreply.github.com>
2022-09-16 02:28:16 +0100

    0-is supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,29 @@
-#include "stdio.h"
+#include "main.h"
+
+
 
 /**
- * _isupper - check for upper case letter
- * @c : character to check the case
- * Return:0 or 1
- */
+*_isupper - checks if a  character is uppercase or not
+*@c: character to be tested
+*Return: 1 whether it is, 0 otherwise
+*/
+
+
 
 int _isupper(int c)
+
 {
-	return (c >= 65 && c <= 90);
+
+	if ((c >= 'A') && (c <= 'Z'))
+
+	{
+
+		return (1);
+
+	}
+
+
+
+	return (0);
 
 }
