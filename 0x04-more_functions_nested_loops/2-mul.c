280876b88f05720bd0e1d510be785c737c912815
Dking2244 <111756648+Dking2244@users.noreply.github.com>
2022-09-17 10:47:44 +0100

    Good

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,16 @@
-#include "main.h"
+ #include "main.h"
+
 /**
- * mul - returns the product of its parameters
- * @a: int type number
- * @b: int type number
- * Return:0
+ * mul - returns result of multiplication between 2 integers
+ *@a: first multiplier
+ *@b: second multiplier
+ *
+ * Return: multiplication result
  */
-
-
 int mul(int a, int b)
 {
-return (a * b);
+	int result;
+
+	result = a * b;
+	return (result);
 }
